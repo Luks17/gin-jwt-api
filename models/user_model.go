50073234ae11cb1324417ac854f9630c873cb348
Luks17 <lucasthidavort@gmail.com>
2022-11-15 09:53:22 -0400

    Fixed errors in user model schemas

--- models/user_model.go
+++ models/user_model.go
@@ -8,11 +8,11 @@ import (
 
 type User struct {
 	ID           primitive.ObjectID `bson:"_id"`
-	Name         *string            `json:"name,omitempty" validate:"required, min=2, max=200"`
+	Name         *string            `json:"name,omitempty" validate:"required,min=2,max=200"`
 	Password     *string            `json:"password,omitempty" validate:"required"`
-	Email        *string            `json:"email,omitempty", validate:"required"`
+	Email        *string            `json:"email,omitempty" validate:"required"`
 	Token        *string            `json:"token"`
-	UserType     *string            `json:"user_type,omitempty", validate:"required, eq=ADMIN|eq=USER"`
+	UserType     *string            `json:"user_type,omitempty" validate:"required,eq=ADMIN|eq=USER"`
 	RefreshToken *string            `json:"refresh_token"`
 	CreatedAt    time.Time          `json:"created_at"`
 	UpdatedAt    time.Time          `json:"updated_at"`
